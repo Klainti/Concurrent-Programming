ec7f27721e004db9ea57c3440ac9b4ad49b5f2a9
Klainti Bontourri <kbontourri@gmail.com>
2016-10-04 18:36:34 +0300

    System pipe with threads!

--- hw1/system_pipe.c
+++ hw1/system_pipe.c
@@ -1,21 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <unistd.h>
+
+int fd[2];
+
+void *read_pipe(void *ptr){
+    
+    char read_byte;
+
+    while(read(fd[0],&read_byte,sizeof(char))){
+        printf("%c", read_byte);
+    }
+
+    close(fd[0]);
+}
+
+void *write_pipe(void *ptr){
+
+    char write_byte;
+    int i;
+
+    write_byte = getchar();
+    while (write_byte!=EOF){
+        write(fd[1],&write_byte,sizeof(char));
+        write_byte = getchar();
+    }
+
+    close(fd[1]);
+}
 
 
 int main(int argc,char *argv[]){
-    
-    int fd[2],write_byte,read_byte;
 
+    pthread_t thread1,thread2;
+    int iret1,iret2;
+    
     pipe(fd);
 
-    write_byte = getchar();
-    while(write_byte!=EOF){
-        write(fd[1],&write_byte,sizeof(char));
-        read(fd[0],&read_byte,sizeof(char));
-        printf("%c",read_byte);
-        write_byte = getchar();
+    iret1 = pthread_create(&thread1,NULL,read_pipe,NULL);
+    if (iret1==1){
+        fprintf(stderr,"Error - pthread_create() return: %d\n",iret1);
+        return(EXIT_FAILURE);
     }
+
+    iret2 = pthread_create(&thread2,NULL,write_pipe,NULL);
+    if (iret2==1){
+        fprintf(stderr,"Error - pthread_create() return: %d\n",iret2);
+        return(EXIT_FAILURE);
+    }
+
+    pthread_join(thread1,NULL);
+    pthread_join(thread2,NULL);
 
     return(0);
 }
