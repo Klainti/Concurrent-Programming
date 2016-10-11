70291b1cc2f59f805f0a66271e82617428848e86
Chris Georgakidis <cgeorgakidis@inf.uth.gr>
2016-10-11 17:45:00 +0300

    Calc depth of recursion / Create 2^k - 1 threads

--- homework1/problem3/quicksort.c
+++ homework1/problem3/quicksort.c
@@ -17,17 +17,12 @@ void *quicksort(void *ptr);
 
 volatile int *arr;  //{5,0,3,80,89,45,7,13,22,54};
 int tableSize;
-int nofRecursion;
-volatile int nofThreads;
-double nofThreadsMax;
+volatile int nofRecursion;
 
 int main(){
 
-    int i,iret,arr_bound[3];
+    int i,iret,arr_bound[4]; //{low,high,thread_done,depth_of_recursion}
     pthread_t main_thread;
-
-    nofThreads = 1; //the main thread
-    printf("Number of Threads: %d\n", nofThreads);
 
     //The size of the table
     printf("Give the size of the table: ");
@@ -37,6 +32,16 @@ int main(){
         printf("The size of table must not be negative or zero.\nPlease try again...");
       }
     }while (tableSize <= 0);
+
+    //The number of the recursion
+    printf("Give the number of recursion:");
+    do{
+      scanf ("%d", &nofRecursion);
+      if (nofRecursion <= 0){
+        printf("The number of recursion must not be negative or zero.\nPlease try again...");
+      }
+    }while (nofRecursion <= 0);
+    printf("Number of Recursion: %d\n", nofRecursion);
 
     //The numbers to sort
     arr = (int*) calloc(tableSize, sizeof(int));
@@ -50,20 +55,12 @@ int main(){
         printf("%d ", arr[i]);
     printf("\n");
 
-    //The number of the recursion
-    printf("Give the number of recursion:");
-    do{
-      scanf ("%d", &nofRecursion);
-      if (nofRecursion <= 0){
-        printf("The number of recursion must not be negative or zero.\nPlease try again...");
-      }
-    }while (nofRecursion <= 0);
-    nofThreadsMax = pow(2, nofRecursion) - 1;
-    printf("Max Threads: %f\n", nofThreadsMax);
+
 
     arr_bound[0]=0;
-    arr_bound[1]=9;
+    arr_bound[1]=tableSize-1;
     arr_bound[2]=1;
+    arr_bound[3]= 1; //depth of recursion
 
 /*TODO
   Remove the pthread_create from main, [OK]
@@ -72,13 +69,13 @@ int main(){
   */
 
     //create thread
-    // iret = pthread_create(&main_thread,NULL,quicksort,(void *)arr_bound);
-    // if (iret){
-    //     fprintf(stderr,"Error - pthread_create() return: %d\n",iret);
-    //     return(EXIT_FAILURE);
-    // }
+    iret = pthread_create(&main_thread,NULL,quicksort,(void *)arr_bound);
+    if (iret){
+        fprintf(stderr,"Error - pthread_create() return: %d\n",iret);
+        return(EXIT_FAILURE);
+    }
 
-    quicksort((void *)arr_bound);
+    //quicksort((void *)arr_bound);
 
     while(arr_bound[2]){sched_yield();};
 
@@ -87,20 +84,18 @@ int main(){
         printf("%d ", arr[i]);
     printf("\n");
 
-    printf("%d\n", nofThreads);
-    return 0;
 }
 
 void *quicksort(void *ptr){
-    int pivot, i, j, temp,iret1,iret2,low,high;
-    int left_bound_terminate[3],right_bound_terminate[3];// 0: low, 1:high, 2:thread terminated
+    int pivot, i, j, temp,iret1,iret2,low,high, currentDepth;
+    int left_bound_terminate[4],right_bound_terminate[4];// {low, high, thread_done, depth of the recursion}
     int *bound; //typecast *ptr
     pthread_t left_thread,right_thread;
 
     bound = (int *)ptr;
     low = bound[0];
     high = bound[1];
-   // printf("%d\n",*((int*)ptr + 3*sizeof(int)));
+    currentDepth = bound[3];
 
     if(low < high) {
         pivot = low; // select a pivot element
@@ -133,35 +128,42 @@ void *quicksort(void *ptr){
         left_bound_terminate[1] = j-1;
         left_bound_terminate[2] = 1; //thread not terminated
 
+
         right_bound_terminate[0] = j+1;
         right_bound_terminate[1] = high;
         right_bound_terminate[2] = 1; //thread not terminated
 
 
-        if ((nofThreads+2) <= nofThreadsMax){
+        // printf("Sorting Array\n");
+        // for(i = 0; i < tableSize; i++)
+        //     printf("%d ", arr[i]);
+        // printf("\n");
+
+        if (currentDepth+1 <= nofRecursion){
+          printf("Current number of Recursion: %d\n", currentDepth);
+          //Increase the depth of the recursion.
+
+          left_bound_terminate[3] = currentDepth + 1;
+          right_bound_terminate[3] = currentDepth + 1;
+
+          //Create the thread for the left subtable.
           iret1 = pthread_create(&left_thread,NULL,quicksort,(void*) left_bound_terminate);
           if (iret1){
               fprintf(stderr,"Error - pthread_create() return: %d\n",iret1);
               return(NULL);
           }
 
+          //Create the thread for the right subtable.
           iret2 = pthread_create(&right_thread,NULL,quicksort,(void*) right_bound_terminate);
           if (iret2){
               fprintf(stderr,"Error - pthread_create() return: %d\n",iret2);
               return(NULL);
           }
-          nofThreads = nofThreads + 2;
         }
-        else if ((nofThreads+1) <= nofThreadsMax){
-          iret1 = pthread_create(&left_thread,NULL,quicksort,(void*) left_bound_terminate);
-          if (iret1){
-              fprintf(stderr,"Error - pthread_create() return: %d\n",iret1);
-              return(NULL);
-          }
-          nofThreads++;
-          quicksort((void*)left_bound_terminate);
-        }
-        else{
+        else{  // if (numberOfRecursion + 1 > nofRecursion)
+          //We must stop create new threads and the quicksort has to continue in one thread.
+          left_bound_terminate[3] = currentDepth;
+          right_bound_terminate[3] = currentDepth;
           quicksort((void*)right_bound_terminate);
           quicksort((void*)left_bound_terminate);
         }
