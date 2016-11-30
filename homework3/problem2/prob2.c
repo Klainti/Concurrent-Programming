d44234959abb84d29a38a128f6143b9f4933fa40
Klainti Bontourri <kbontourri@gmail.com>
2016-11-30 16:05:51 +0200

    Fix print at same line with debug_mtx

--- homework3/problem2/prob2.c
+++ homework3/problem2/prob2.c
@@ -6,7 +6,7 @@
 
 #include "debug.h"
 
-pthread_mutex_t mtx;
+pthread_mutex_t mtx, debug_mtx;
 pthread_cond_t startTrain, embarkate, debarkate;
 int nOfPassenger=0, N = 0, trainStarted = 0, trainFinished = 0;
 int blockedEmbarkation = 0, blockedDebarkation = 0;
@@ -36,6 +36,7 @@ int main(int argc,char *argv[]){
 
     debug("Init mtx at 1");
     pthread_mutex_init(&mtx, NULL);
+    pthread_mutex_init(&debug_mtx, NULL);
 
     pthread_cond_init(&startTrain, NULL);
     pthread_cond_init(&embarkate, NULL);
@@ -69,24 +70,34 @@ int main(int argc,char *argv[]){
 void *train(){
 
     while(1){
+    	pthread_mutex_lock(&debug_mtx);
         debug("Train is entering monitor to wait to be full");
+        pthread_mutex_unlock(&debug_mtx);
         pthread_mutex_lock(&mtx);
         trainStart();
+        pthread_mutex_lock(&debug_mtx);
         debug("Train exits monitor");
+        pthread_mutex_unlock(&debug_mtx);
         pthread_mutex_unlock(&mtx);
+        pthread_mutex_lock(&debug_mtx);
         debug("Train is about to begin");
+        pthread_mutex_unlock(&debug_mtx);
         sleep(2);
+        pthread_mutex_lock(&debug_mtx);
         debug("Ride is over");
         debug("Train is entering monitor to empty");
+        pthread_mutex_unlock(&debug_mtx);
         pthread_mutex_lock(&mtx);
         trainStop();
-        pthread_mutex_unlock(&mtx); 
+        pthread_mutex_unlock(&mtx);
+        pthread_mutex_lock(&debug_mtx);
         if (trainStarted == 0){
             debug("Train is empty and exits monitor");
         }
         else{
             debug("Train exits monitor and passengers are debarkating");
         }
+        pthread_mutex_unlock(&debug_mtx);
     }
 
     return NULL;
@@ -96,20 +107,34 @@ void *passenger(void *arg){
     int id;
     id = *(int *)arg;
 
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d is entering monitor to embarkate", id);
+    pthread_mutex_unlock(&debug_mtx);
     pthread_mutex_lock(&mtx);
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d is going to embarkate", id);
+    pthread_mutex_unlock(&debug_mtx);
     embarkation(id);
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d embarkated and exits the monitor", id);
+    pthread_mutex_unlock(&debug_mtx);
     pthread_mutex_unlock(&mtx);
     //Passenger wait for train to finish its ride to debarkate
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d is entering monitor to debarkate", id);
+    pthread_mutex_unlock(&debug_mtx);
     pthread_mutex_lock(&mtx);
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d is going to debarkate", id);
+    pthread_mutex_unlock(&debug_mtx);
     debarkation(id);
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d debarkated and exits the monitor", id);
+    pthread_mutex_unlock(&debug_mtx);
     pthread_mutex_unlock(&mtx);
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d is done", id);
+    pthread_mutex_unlock(&debug_mtx);
 
     return NULL;
 }
@@ -117,17 +142,23 @@ void *passenger(void *arg){
 void trainStart(){
     //debug("trainStarted = %d || trainFinished = %d", trainStarted, trainFinished);
     if(trainStarted == 0 || trainFinished == 1){
+    	pthread_mutex_lock(&debug_mtx);
         debug("Train waits to full");
+        pthread_mutex_unlock(&debug_mtx);
         pthread_cond_wait(&startTrain, &mtx);
     }
     else {
+    	pthread_mutex_lock(&debug_mtx);
         debug("Train is full. Ride is going to start");
+        pthread_mutex_unlock(&debug_mtx);
     }
 }
 
 void trainStop(){
     trainFinished = 1;
+    pthread_mutex_lock(&debug_mtx);
     debug("Debarkation is about to begin");
+    pthread_mutex_unlock(&debug_mtx);
     pthread_cond_signal(&debarkate);
 }
 
@@ -136,22 +167,30 @@ void embarkation(int id){
         /*Train is started.
         Other passengers must wait until train finish its ride. */
         blockedEmbarkation++;
+        pthread_mutex_lock(&debug_mtx);
         debug("Passenger %d waits for train to finish its ride", id);
+        pthread_mutex_unlock(&debug_mtx);
         pthread_cond_wait(&embarkate, &mtx);
     }
     nOfPassenger++;
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d is on train. Total number of passengers: %d",id, nOfPassenger);
+    pthread_mutex_unlock(&debug_mtx);
     if(nOfPassenger == N){
         /*Train is full.
         Notify train to start the ride */
+        pthread_mutex_lock(&debug_mtx);
         debug("Last passenger entered the train.Start the train");
+        pthread_mutex_unlock(&debug_mtx);
         trainStarted = 1;
         pthread_cond_signal(&startTrain);
     }
     else{
         if(blockedEmbarkation != 0){
             blockedEmbarkation--;
+            pthread_mutex_lock(&debug_mtx);
             debug("Passenger %d wakes up other blocked passenger", id);
+            pthread_mutex_unlock(&debug_mtx);
             pthread_cond_signal(&embarkate);
         }
     }
@@ -161,16 +200,22 @@ void debarkation(int id){
     //debug("Passenger %d waits for train to finish its ride %d", id);
     if(trainFinished == 0){
         blockedDebarkation++;
+        pthread_mutex_lock(&debug_mtx);
         debug("Passenger %d waits for train to finish its ride, to debarkate", id);
+        pthread_mutex_unlock(&debug_mtx);
         pthread_cond_wait(&debarkate, &mtx);
     }
 
     nOfPassenger--;
+    pthread_mutex_lock(&debug_mtx);
     debug("Passenger %d debarkated. Remaining passengers: %d", id, nOfPassenger);
+    pthread_mutex_unlock(&debug_mtx);
     if (nOfPassenger == 0){
         /*Last passenger out of the train.
         Notify other passenger to embarkate */
+        pthread_mutex_lock(&debug_mtx);
         debug("Last passenger (id = %d) debarkated", id);
+        pthread_mutex_unlock(&debug_mtx);
         trainStarted = 0;
         if (blockedEmbarkation != 0){
             blockedEmbarkation--;
